3d568d0fcfcda0997138fec0ac87e81250ca1745
nasjones <nasjonez1@gmail.com>
2020-08-13 13:31:02 -0400

    blog added and link updates

--- blog/models.py
+++ blog/models.py
@@ -7,4 +7,15 @@ class Blog(models.Model):
     title = models.CharField(max_length=50)
     image = models.ImageField(upload_to='images/')
     pub_date = models.DateTimeField()
-    body = models.CharField(max_length=200)
+    body = models.TextField()
+
+    def __str__(self):
+        return self.title, self.pub_date.strftime('%b %e, %Y, %I:%M')
+
+    def summary(self):
+        if len(self.body) > 100:
+            return self.body[:100] + '...'
+        return self.body
+
+    def pub_date_pretty(self):
+        return self.pub_date.strftime('%b %e, %Y')
--- pythonportfolio/settings.py
+++ pythonportfolio/settings.py
@@ -111,7 +111,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'US/EASTERN'
 
 USE_I18N = True
 
@@ -122,6 +122,12 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'pythonportfolio/static/')
+]
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 STATIC_URL = '/static/'
 
